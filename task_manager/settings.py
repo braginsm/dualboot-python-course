49c65e4880e33814c7d49961a9d5bfa18f034589
Sergei Bragin <braginsm@gmail.com>
2023-06-08 15:59:23 +0300

    7.4 Configure API to work only with authorized mode

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -138,4 +138,5 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }

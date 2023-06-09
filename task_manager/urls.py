a948edf9260ed9a4bed90f000e77fa2438130efb
Sergei Bragin <braginsm@gmail.com>
2023-06-09 06:21:34 +0000

    7.5 tests

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -44,6 +44,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", task_manager_admin_site.urls),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include(router.urls)),
     re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
@@ -58,6 +60,4 @@ urlpatterns = [
     re_path(
         r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
     ),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]

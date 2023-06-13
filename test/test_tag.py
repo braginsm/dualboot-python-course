cb25a0cbaa6e9d9674ce5ca91d9efec77edebdfa
Sergei Bragin <braginsm@gmail.com>
2023-06-13 09:51:57 +0000

    8.2 fix tests

--- test/test_tag.py
+++ test/test_tag.py
@@ -15,41 +15,47 @@ class TestTagViewSet(TestViewSetBase):
         return {"id": entity["id"], **attributes}
 
     def test_create(self):
+        self.login()
         tag = self.create(self.tag_attributes)
         expected_response = self.expected_details(tag, self.tag_attributes)
         assert tag == expected_response
 
     def test_get_by_id(self):
+        self.login()
         tag = self.create(self.tag_attributes)
         tag_id = tag["id"]
         compare_tag = self.get_by_id(tag_id)
         assert compare_tag == self.expected_details(tag, self.tag_attributes)
 
     def test_get_tags(self):
+        self.login()
         tag = self.create(self.tag_attributes)
         response = self.get_list()
         tags = self.response_to_list_dict(response)
         assert self.expected_details(tag, self.tag_attributes) in tags
 
     def test_put_tag(self):
+        self.login()
         tag = self.create(self.tag_attributes)
         expected = {**tag, **self.edit_fields}
         new_tag = self.put_by_id(expected, tag["id"])
         assert new_tag == expected
 
     def test_patch_tag(self):
+        self.login()
         tag = self.create(self.tag_attributes)
         expected = {**tag, **self.edit_fields}
         new_tag = self.patch_by_id(self.edit_fields, tag["id"])
         assert new_tag == expected
 
     def test_delete_tag(self):
+        self.login()
         tag = self.create(self.tag_attributes)
         tag_id = tag["id"]
         assert self.delete_by_id(tag_id)
         assert not self.delete_by_id(tag_id)
 
     def test_delete_tag_without_permission(self):
+        self.login(is_staff=False)
         tag = self.create(self.tag_attributes)
-        user = User.objects.create_user("test2@test.ru", email=None, password=None)
-        assert not self.delete_by_id(tag["id"], user)
+        assert not self.delete_by_id(tag["id"])
--- test/test_user.py
+++ test/test_user.py
@@ -20,35 +20,41 @@ class TestUserViewSet(TestViewSetBase):
         return {"id": entity["id"], **attributes}
 
     def test_create(self):
+        self.login()
         user = self.create(self.user_attributes)
         expected_response = self.expected_details(user, self.user_attributes)
         assert user == expected_response
 
     def test_get_by_id(self):
+        self.login()
         user = self.create(self.user_attributes)
         user_id = user["id"]
         compare_user = self.get_by_id(user_id)
         assert compare_user == self.expected_details(user, self.user_attributes)
 
     def test_get_users(self):
+        self.login()
         user = self.create(self.user_attributes)
         response = self.get_list()
         users = self.response_to_list_dict(response)
         assert self.expected_details(user, self.user_attributes) in users
 
     def test_put_user(self):
+        self.login()
         user = self.create(self.user_attributes)
         expected = {**user, **self.edit_fields}
         new_user = self.put_by_id(expected, user["id"])
         assert new_user == expected
 
     def test_patch_user(self):
+        self.login()
         user = self.create(self.user_attributes)
         expected = {**user, **self.edit_fields}
         new_user = self.patch_by_id(self.edit_fields, user["id"])
         assert new_user == expected
 
     def test_delete_user(self):
+        self.login()
         user = self.create(self.user_attributes)
         user_id = user["id"]
         assert self.delete_by_id(user_id)
